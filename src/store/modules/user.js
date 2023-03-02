c192eb352b1f155082e91585b7b1dc39467e656f
donghao <408097591@qq.com>
2023-03-02 20:28:41 +0800

    login接口

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -39,9 +39,11 @@ const user = {
     Login ({ commit }, userInfo) {
       return new Promise((resolve, reject) => {
         login(userInfo).then(response => {
-          const result = response.result
-          storage.set(ACCESS_TOKEN, result.token, new Date().getTime() + 7 * 24 * 60 * 60 * 1000)
-          commit('SET_TOKEN', result.token)
+          if (response.errorCode === '200') {
+              const token = response.content
+              storage.set(ACCESS_TOKEN, token, new Date().getTime() + 7 * 24 * 60 * 60 * 1000)
+              commit('SET_TOKEN', token)
+          }
           resolve()
         }).catch(error => {
           reject(error)
